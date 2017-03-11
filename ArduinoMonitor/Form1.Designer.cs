8008d7ded5596c75b982bf5bec5f2dca5e16c845
Giovanni Scotti <gio.scotti.94@gmail.com>
2017-03-11 22:30:52 +0100

    Add project files to repository

---
+++ ArduinoMonitor/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ArduinoMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.textBaud = new System.Windows.Forms.TextBox();
+            this.labelPort = new System.Windows.Forms.Label();
+            this.labelBaud = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialList = new System.Windows.Forms.ComboBox();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(59, 174);
+            this.buttonConnect.Margin = new System.Windows.Forms.Padding(50, 3, 10, 30);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(206, 43);
+            this.buttonConnect.TabIndex = 1;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Enabled = false;
+            this.buttonDisconnect.Location = new System.Drawing.Point(59, 240);
+            this.buttonDisconnect.Margin = new System.Windows.Forms.Padding(50, 3, 3, 50);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(438, 43);
+            this.buttonDisconnect.TabIndex = 2;
+            this.buttonDisconnect.Text = "Disconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // textBaud
+            // 
+            this.textBaud.Location = new System.Drawing.Point(291, 106);
+            this.textBaud.Margin = new System.Windows.Forms.Padding(3, 3, 50, 30);
+            this.textBaud.Name = "textBaud";
+            this.textBaud.Size = new System.Drawing.Size(206, 31);
+            this.textBaud.TabIndex = 3;
+            this.textBaud.Text = "9600";
+            // 
+            // labelPort
+            // 
+            this.labelPort.AutoSize = true;
+            this.labelPort.Location = new System.Drawing.Point(54, 43);
+            this.labelPort.Margin = new System.Windows.Forms.Padding(3, 0, 3, 30);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(118, 25);
+            this.labelPort.TabIndex = 4;
+            this.labelPort.Text = "Serial Port:";
+            this.labelPort.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelBaud
+            // 
+            this.labelBaud.AutoSize = true;
+            this.labelBaud.Location = new System.Drawing.Point(54, 109);
+            this.labelBaud.Name = "labelBaud";
+            this.labelBaud.Size = new System.Drawing.Size(119, 25);
+            this.labelBaud.TabIndex = 5;
+            this.labelBaud.Text = "Baud Rate:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // serialList
+            // 
+            this.serialList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.serialList.FormattingEnabled = true;
+            this.serialList.Location = new System.Drawing.Point(291, 40);
+            this.serialList.Margin = new System.Windows.Forms.Padding(3, 3, 3, 30);
+            this.serialList.Name = "serialList";
+            this.serialList.Size = new System.Drawing.Size(206, 33);
+            this.serialList.TabIndex = 6;
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(291, 174);
+            this.buttonRefresh.Margin = new System.Windows.Forms.Padding(10, 3, 50, 30);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(206, 43);
+            this.buttonRefresh.TabIndex = 7;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(556, 316);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.serialList);
+            this.Controls.Add(this.labelBaud);
+            this.Controls.Add(this.labelPort);
+            this.Controls.Add(this.textBaud);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.buttonConnect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Arduino Monitor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.TextBox textBaud;
+        private System.Windows.Forms.Label labelPort;
+        private System.Windows.Forms.Label labelBaud;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox serialList;
+        private System.Windows.Forms.Button buttonRefresh;
+    }
+}
+
---
+++ ArduinoMonitor/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoMonitor
+{
+    public partial class Form1 : Form
+    {
+        private PerformanceCounter cpuCounter;
+        private PerformanceCounter ramCounter;
+
+        private SerialPort port;
+        private int baudRate;
+        private String serialPortName;
+
+        private String usedCpu;
+        private String usedRam;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCpuCounter();
+            InitializeRamCounter();
+
+            String[] portsList = SerialPort.GetPortNames();
+            serialList.DataSource = portsList;
+        }
+
+        private void InitializeCpuCounter()
+        {
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        }
+  
+        private void InitializeRamCounter()
+        {
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // When Connect button is pressed...
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPortName = serialList.SelectedItem.ToString();
+                baudRate = Convert.ToInt32(textBaud.Text);
+
+                port = new SerialPort(serialPortName);
+                port.BaudRate = baudRate;
+                port.Open();
+
+                if (port.IsOpen)
+                {
+                    timer1.Enabled = true;
+                    buttonConnect.Enabled = false;
+                    buttonDisconnect.Enabled = true;
+                }
+            } catch(NullReferenceException exc)
+            {
+                MessageBox.Show("Select a Serial Port!", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+            } catch(System.FormatException exc)
+            {
+                MessageBox.Show("Set a Baud Rate!", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        // When Disconnect button is pressed...
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            // Close the serial port
+            port.Close();
+            buttonConnect.Enabled = true;
+            buttonDisconnect.Enabled = false;
+        }
+
+        // Update Cpu and Ram usage
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //getComponent("Win32_Processor", "CurrentClockSpeed");
+
+            // Send Cpu usage over the serial line
+            //usedCpu = ((int)cpuCounter.NextValue()).ToString();
+            //port.WriteLine(usedCpu);
+
+            // Send Ram usage over the serial line
+            usedRam = ((int)ramCounter.NextValue()).ToString();
+            port.WriteLine(usedRam);
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            String[] portsList = SerialPort.GetPortNames();
+            serialList.DataSource = portsList;
+        }
+
+        static private void getComponent(String hwclass, String syntax)
+        {
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM " + hwclass);
+            foreach (ManagementObject mj in mos.Get())
+            {
+                Console.WriteLine(Convert.ToString(mj[syntax]));
+            }
+        }
+
+    }
+}
